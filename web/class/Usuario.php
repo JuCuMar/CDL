489c07191bf9593719545e237ca14fbfabeac81e
Justo <jufeol@alumnatflorida.es>
2025-05-06 21:46:00 +0200

    añadiendo producto

--- web/class/Usuario.php
+++ web/class/Usuario.php
@@ -2,24 +2,28 @@
 
     class Usuario {
 
+        private $ID;
         private $nombre;
         private $correo;
         private $contraseña;
-        private $ID;
         private $fecha_nac;
         private $direccion;
         private $telefono;
 
         public function __construct($name = "", $email = "", $pw = "", $id = "", $date = "", $adress = "", $phone = ""){
+            $this->ID = $id;
             $this->nombre = $name;
             $this->correo = $email;
             $this->contraseña = $pw;
-            $this->ID = $id;
             $this->fecha_nac = $date;
             $this->direccion = $adress;
             $this->telefono = $phone;
         }
     
+        public function getID(){
+            return $this->ID;
+        }
+
         public function getNombre(){
             return $this->nombre;
         }
@@ -31,11 +35,7 @@
         public function getContraseña(){
             return $this->contraseña;
         }
-
-        public function getID(){
-            return $this->ID;
-        }
-
+        
         public function getFechaNac(){
             return $this->fecha_nac;
         }
