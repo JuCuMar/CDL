ca9f70214e17f713c4ba047905ffcf7927089234
Justo <jufeol@alumnatflorida.es>
2025-05-07 11:27:18 +0200

    subiendo cambios

--- web/videojuegos.php
+++ web/videojuegos.php
@@ -24,54 +24,78 @@
         </form>
     </nav>
 
-
-
+    <section id="section_videojuegos">
+        
+        <div class="container_videojuegos">
+            <div class="recuadro"><img class="miniatura" src="img/img_predet.jpg"></div>
+            <div class="texto_noticia"></div>
+        </div>
+        <div class="container_videojuegos">
+            <div class="recuadro"><img class="miniatura" src="img/img_predet.jpg"></div>
+            <div class="texto_noticia"></div>
+        </div>
+        <div class="container_videojuegos">
+            <div class="recuadro"><img class="miniatura" src="img/img_predet.jpg"></div>
+            <div class="texto_noticia"></div>
+        </div>
+        <div class="container_videojuegos">
+            <div class="recuadro"><img class="miniatura" src="img/img_predet.jpg"></div>
+            <div class="texto_noticia"></div>
+        </div>
+        <div class="container_videojuegos">
+            <div class="recuadro"><img class="miniatura" src="img/img_predet.jpg"></div>
+            <div class="texto_noticia"></div>
+        </div>
+        <div class="container_videojuegos">
+            <div class="recuadro"><img class="miniatura" src="img/img_predet.jpg"></div>
+            <div class="texto_noticia"></div>
+        </div>
+        
+    </section>
   
     </body>
 
     <footer>
-        <div class="footer-row">
-            <div class="footer-links">
-                <h4>Nosotros</h4>
-                <ul>
-                    <li><a href="#contacto">Contáctanos</a></li>
-                    <li><a href="#about-us">Sobre nosotros</a></li>
-                    <li><a href="#politica">Política de privacidad</a></li>
-                </ul>
-            </div>
-
-            <div class="footer-links">
-                <h4>Ayuda</h4>
-                <ul>
-                    <li><a href="#faq">Preguntas comunes</a></li>
-                    <li><a href="#sugerencias">Sugerencias</a></li>
-                </ul>
-            </div>
-
-            <div class="footer-img">
-                <h4>Síguenos</h4>
-                <a href="https://instagram.com"><img src="img/ig.png"></a>
-                <a href="https://twitter.com"><img src="img/tw.png"></a>
-                <a href="https://facebook.com"><img src="img/fb.png"></a>
-            </div>
-
-            <div class="footer-img">
-                <img id="pato" src="img/pato.png" alt="Pato">
-                <audio id="sound" src="pato.mp3" preload="none"></audio>
-            </div>
+    <div class="footer-row">
+        <div class="footer-links">
+            <h4>Nosotros</h4>
+            <ul>
+                <li><a href="#contacto">Contáctanos</a></li>
+                <li><a href="#about-us">Sobre nosotros</a></li>
+                <li><a href="#politica">Política de privacidad</a></li>
+            </ul>
         </div>
-        <br>
-        <h6>© 2025 CRIPTA-DEL-LORE</h6>
-        </div>
-    </footer>
-<script>
-    const pato = document.getElementById('pato');
-    const sound = document.getElementById('sound');
 
-    pato.addEventListener('click', () => {
-        sound.play();
-    });
+        <div class="footer-links">
+            <h4>Ayuda</h4>
+            <ul>
+                <li><a href="#faq">Preguntas comunes</a></li>
+                <li><a href="#sugerencias">Sugerencias</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-img">
+            <h4>Síguenos</h4>
+            <a href="https://instagram.com"><img src="img/ig.png"></a>
+            <a href="https://twitter.com"><img src="img/tw.png"></a>
+            <a href="https://facebook.com"><img src="img/fb.png"></a>
+        </div>
+
+        <div class="footer-img">
+            <img id="pato" src="img/pato.png" alt="Pato">
+            <audio id="sound" src="pato.mp3" preload="none"></audio>
+        </div>
+    </div>
+    <br>
+    <h6>© 2025 CRIPTA-DEL-LORE</h6>  
+</footer>
+
+    <script>
+        const pato = document.getElementById('pato');
+        const sound = document.getElementById('sound');
+
+        pato.addEventListener('click', () => {
+            sound.play();
+        });
     </script>
-</body>
-
 </html>
