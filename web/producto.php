ee47a56c44e361547d5aac912b2a0e2bcca17e10
Justo <jufeol@alumnatflorida.es>
2025-05-08 11:34:42 +0200

    necesito testear

--- web/producto.php
+++ web/producto.php
@@ -38,7 +38,7 @@ require_once('autoloader.php');
     
     <section>
         <div class="container_info">
-            <div class="portada"><img src="img/nov3.jpg"></div>
+            <div class="portada"><img src="img/videojuegos/vid1.jpg"></div>
             <div class=""><h2>ER VARDUS GATE TRES PREEEMO</h2></div>
         </div>
         <h2>DESCRIPCIÓN</h2>
@@ -82,7 +82,7 @@ require_once('autoloader.php');
     </div>
     <br>
     <h6>© 2025 CRIPTA-DEL-LORE</h6>  
-</footer>
+    </footer>
 
     <script>
         const pato = document.getElementById('pato');
